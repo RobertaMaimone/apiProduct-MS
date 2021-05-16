f6db676b0cbffa5f40a0854cfeb6cc1d336f6855
RobertaMaimone <59478805+RobertaMaimone@users.noreply.github.com>
2021-05-16 14:53:16 -0300

    Delete RegisteredProduct.java

--- src/main/java/RobertaMaimone/com/github/exception/RegisteredProduct.java
+++
@@ -1,8 +0,0 @@
-package RobertaMaimone.com.github.exception;
-
-public class RegisteredProduct extends RuntimeException{
-
-    public RegisteredProduct(String nameProduct){
-        super("Produto já cadastrado" + nameProduct);
-    }
-}
